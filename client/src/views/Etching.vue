ba324d171b6540fb76a4af2e008f8e01272688d2
lawsberryPi <shawn-yu@huskers.unl.edu>
2019-08-07 21:31:27 -0400

    added glowler

--- client/src/views/Etching.vue
+++ client/src/views/Etching.vue
@@ -1,5 +1,46 @@
 <template>
-    <p>Hello from glass etching busniess</p>
+    <v-card
+      max-width="100%"
+      max-height="100%"
+      class="mx-auto"
+    >
+      <v-layout
+        py-4
+        pl-4
+      >
+        <v-flex shrink>
+          <v-img
+            height="600"
+            width="auto"
+            :src="require(`@/assets/etching/growler.jpg`)"
+          ></v-img>
+        </v-flex>
+        <v-flex text-center>
+          <v-container
+            grid-list-lg
+            pa-0
+          >
+            <v-layout column>
+              <v-flex>
+                <v-btn icon>
+                  <v-icon>mdi-heart</v-icon>
+                </v-btn>
+              </v-flex>
+              <v-flex>
+                <v-btn icon>
+                  <v-icon>mdi-bookmark</v-icon>
+                </v-btn>
+              </v-flex>
+              <v-flex>
+                <v-btn icon>
+                  <v-icon>mdi-share-variant</v-icon>
+                </v-btn>
+              </v-flex>
+            </v-layout>
+          </v-container>
+        </v-flex>
+      </v-layout>
+    </v-card>
 </template>
 <script>
 export default {
